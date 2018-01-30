7994f8f8cf4a35aa986d4c15ebef440f94729797
alez <alez1986@gmail.com>
2018-01-30 07:47:35 +0300

    Initial commit

---
+++ app/src/main/java/com/example/android/quizapp/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.android.quizapp;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    void submit() {
+        int total = question_1() + question_2() + question_3() +question_4() + question_5();
+        String message = getString(R.string.score, total);
+        
+        Context context = getApplicationContext();
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, message, duration);
+        toast.show();
+    }
+
+    int question_1() {
+        CheckBox moscowCheckBox = (CheckBox) findViewById(R.id.moscow_1);
+        CheckBox peterCheckBox = (CheckBox) findViewById(R.id.peter_1);
+        CheckBox sochiCheckBox = (CheckBox) findViewById(R.id.sochi_1);
+        CheckBox samaraCheckBox = (CheckBox) findViewById(R.id.samara_1);
+        CheckBox kazanCheckBox = (CheckBox) findViewById(R.id.kazan_1);
+
+        if (moscowCheckBox.isChecked() && sochiCheckBox.isChecked() && !peterCheckBox.isChecked()
+                && !samaraCheckBox.isChecked() && !kazanCheckBox.isChecked())
+            return 1;
+        else
+            return 0;
+    }
+
+    int question_2() {
+        RadioButton radioButton = (RadioButton) findViewById(R.id.radio_12);
+        return radioButton.isChecked() ? 1 : 0;
+    }
+
+    int question_3() {
+        EditText editText = (EditText) findViewById(R.id.capital_edit);
+        return editText.getText().toString().compareToIgnoreCase(getString(R.string.moscow));
+    }
+
+    int question_4() {
+        RadioButton radioButton = (RadioButton) findViewById(R.id.radio_11z);
+        return radioButton.isChecked() ? 1 : 0;
+    }
+
+    int question_5() {
+        RadioButton radioButton = (RadioButton) findViewById(R.id.radio_peter);
+        return radioButton.isChecked() ? 1 : 0;
+    }
+
+
+}
