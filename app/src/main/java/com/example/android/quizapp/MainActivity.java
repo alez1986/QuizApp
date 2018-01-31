808db78b48cfd715314986e1b48bbcbd1e904477
alez <alez1986@gmail.com>
2018-02-01 00:24:34 +0300

    Added icon, finish color theme, hidden keyboard on start and change orientation

--- app/src/main/java/com/example/android/quizapp/MainActivity.java
+++ app/src/main/java/com/example/android/quizapp/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.android.quizapp;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.Toast;
 
@@ -14,12 +19,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DisplayMetrics displaymetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
+        int height = displaymetrics.heightPixels;
+        ImageView image = (ImageView) findViewById(R.id.russia_icon);
+        image.setMaxHeight(height / 2);
     }
 
-    void submit() {
+    void submit(View view) {
         int total = question_1() + question_2() + question_3() +question_4() + question_5();
         String message = getString(R.string.score, total);
-        
+
         Context context = getApplicationContext();
         int duration = Toast.LENGTH_SHORT;
         Toast toast = Toast.makeText(context, message, duration);
@@ -47,7 +58,7 @@ public class MainActivity extends AppCompatActivity {
 
     int question_3() {
         EditText editText = (EditText) findViewById(R.id.capital_edit);
-        return editText.getText().toString().compareToIgnoreCase(getString(R.string.moscow));
+        return editText.getText().toString().compareToIgnoreCase(getString(R.string.moscow)) == 0 ? 1 : 0;
     }
 
     int question_4() {
