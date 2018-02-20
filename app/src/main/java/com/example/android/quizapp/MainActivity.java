f62eee3f98e383340c24b58d9071bbb48c2fc09c
alez <alez1986@gmail.com>
2018-02-20 08:04:01 +0300

    Delete unused imports

--- app/src/main/java/com/example/android/quizapp/MainActivity.java
+++ app/src/main/java/com/example/android/quizapp/MainActivity.java
@@ -1,8 +1,6 @@
 package com.example.android.quizapp;
 
-import android.app.Activity;
 import android.content.Context;
-import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
